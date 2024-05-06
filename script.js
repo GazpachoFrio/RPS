2ced0c514146b586e0603a95da962376fda0468d
GazpachoFrio <ismaelmgo@gmail.com>
2024-05-06 10:28:47 +0200

    getcomputerCHoice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Heya")
+
+ let getComputerChoice = getRandomInt(3);
+    if (getRandomInt === 2) {
+        return "Scissors"
+    } 
+    else if (getRandomInt === 1) {
+        return "Rock"
+    }
+    else if (getRandomInt ===0) {
+        return "Paper"
+    }
+    
+ 
+ 
