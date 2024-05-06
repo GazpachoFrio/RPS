2ac389ae6866a1f514d300484ed6c94c8320cb36
GazpachoFrio <ismaelmgo@gmail.com>
2024-05-06 10:35:10 +0200

    puntuation

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ console.log("Heya")
 
  let getComputerChoice = getRandomInt(3);
     if (getRandomInt === 2) {
-        return "Scissors"
+        return "Scissors";
     } 
     else if (getRandomInt === 1) {
-        return "Rock"
+        return "Rock";
     }
     else if (getRandomInt ===0) {
-        return "Paper"
+        return "Paper";
     }
     console.log("Elijo...", getComputerChoice )
  
